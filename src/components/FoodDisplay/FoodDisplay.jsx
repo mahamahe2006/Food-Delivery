9ac6a5c695e7bd5b9f7a252cd8af9c10c5422eec
mahamahe2006 <mahamahe2006@gmail.com>
2025-12-23 18:21:45 +0530

    footer

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -11,12 +11,13 @@ const FoodDisplay = ({category}) => {
       <h2>Top Dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item,index)=>{
-            if(category="All"|| category===item.category){
+            if(category==="All"|| category===item.category){
                 
             }
             return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
         })}
       </div>
+
     </div>
   )
 }
